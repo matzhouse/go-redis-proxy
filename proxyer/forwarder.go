814c1cd50c96106cefb8691f07d85b933bd388b0
John Doe <mat@crzy.co.uk>
2013-11-19 08:16:30 +0000

    laptop changes

--- proxyer/forwarder.go
+++ proxyer/forwarder.go
@@ -3,6 +3,7 @@ package proxyer
 import (
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 	"time"
@@ -11,11 +12,25 @@ import (
 func Forward(local net.Conn, remoteAddr string) (fan chan int) {
 
 	timeout := time.Second
+	fmt.Println("Connecting to ", remoteAddr)
+
 	remote, err := net.DialTimeout("tcp", remoteAddr, timeout)
 
-	fan = make(chan int)
+	var buffer []byte
+
+	if Canread(remote, buffer) == false {
+		log.Fatal("Redis server not available")
+		Closer(remote)
+	}
 
 	defer Closer(remote)
+
+	if err != nil {
+		fmt.Println(err)
+		Closer(local)
+	}
+
+	fan = make(chan int)
 
 	if remote == nil {
 		fmt.Fprintf(os.Stderr, "remote dial failed: %v\n", err)
@@ -32,4 +47,22 @@ func Closer(conn net.Conn) {
 
 	conn.Close()
 
+}
+
+func Canread(c net.Conn, buffer []byte) bool {
+
+	var ping []byte
+
+	ping = []byte("PING")
+
+	c.Write(ping)
+
+	bytesRead, err := c.Read(buffer)
+	if err != nil {
+		c.Close()
+		log.Println(err)
+		return false
+	}
+	log.Println("Read ", bytesRead, " bytes")
+	return true
 }
